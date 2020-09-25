38116ae977be63486022d186d3d0f53f7f52e0f5
swalschuler <alschuler.s@gmail.com>
2020-09-24 17:35:31 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import PeriodDisplay from "./components/PeriodDisplay";
+
+export default function App() {
+  return (
+    <div>
+      <PeriodDisplay />
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
