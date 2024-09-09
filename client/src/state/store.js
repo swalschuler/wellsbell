8ce973a8836456e3183125f032d4dd8230dd77c4
Schuyler Alschuler <alschuler.s@gmail.com>
2024-09-08 21:15:33 -0700

    Persist toggle

--- client/src/state/store.js
+++ client/src/state/store.js
@@ -1,10 +1,16 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useStore = create((set) => ({
-  checked: false,
-  toggle: () => {
-    set((state) => ({ checked: !state.checked }));
-  },
-}));
+const useStore = create(
+  persist(
+    (set) => ({
+      checked: false,
+      toggle: () => {
+        set((state) => ({ checked: !state.checked }));
+      },
+    }),
+    { name: "wells-bell" }
+  )
+);
 
 export default useStore;
